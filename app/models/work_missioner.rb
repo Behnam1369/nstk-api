5c184054139bc70475cb8f1010309dcb50cb8f1d
behnam1369 <behnam.aghaali@yahoo.com>
2023-01-14 17:57:55 +0330

    Add nested relationships for WorkMissioner model

--- app/models/work_missioner.rb
+++ app/models/work_missioner.rb
@@ -1,6 +1,10 @@
 class WorkMissioner < ApplicationRecord
   belongs_to :user, foreign_key: 'IdUser'
   belongs_to :work_mission, foreign_key: 'IdWorkMission'
+  has_many :work_mission_reports, foreign_key: 'IdWorkMissioner'
+  accepts_nested_attributes_for :work_mission_reports
+  has_many :work_mission_achievements, foreign_key: 'IdWorkMissioner'
+  accepts_nested_attributes_for :work_mission_achievements
 
   self.table_name = 'WorkMissioner'
   self.primary_key = 'IdWorkMissioner'
