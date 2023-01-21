ee71ff52fd6ed90a67f76032baeec123b1eed2ef
behnam1369 <behnam.aghaali@yahoo.com>
2023-01-21 13:42:25 +0330

    Update WorkMission model

--- app/models/work_missioner.rb
+++ app/models/work_missioner.rb
@@ -1,9 +1,9 @@
 class WorkMissioner < ApplicationRecord
   belongs_to :user, foreign_key: 'IdUser'
   belongs_to :work_mission, foreign_key: 'IdWorkMission'
-  has_many :work_mission_reports, foreign_key: 'IdWorkMissioner'
+  has_many :work_mission_reports, foreign_key: 'IdWorkMissioner', dependent: :destroy
   accepts_nested_attributes_for :work_mission_reports
-  has_many :work_mission_achievements, foreign_key: 'IdWorkMissioner'
+  has_many :work_mission_achievements, foreign_key: 'IdWorkMissioner', dependent: :destroy
   accepts_nested_attributes_for :work_mission_achievements
 
   self.table_name = 'WorkMissioner'
