c59300a89ce0cc9c6c28c134d765265c91eddc87
behnam1369 <behnam.aghaali@yahoo.com>
2023-04-05 13:55:52 +0430

    Update Pi model

--- app/models/pi.rb
+++ app/models/pi.rb
@@ -1,6 +1,7 @@
 class Pi < ApplicationRecord
     has_many :pi_itms, foreign_key: 'IdPiItm'
-    belongs_to :delivery_term, class: dl, foreign_key: 
+    has_many :pi_prints, foreign_key: 'IdPi'
+    belongs_to :delivery_term, class: dl, foreign_key: 'IdDeliveryTerm'
 
     self.table_name = 'Pi'
     self.primary_key = 'IdPi'
