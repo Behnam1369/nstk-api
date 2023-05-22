b56b7de6bab4b7bf8a46299f35e9dc357844b98f
jamesbaylor <jamesbay829@gmail.com>
2023-05-22 17:33:13 +0430

    Update Dept model

--- app/models/dept.rb
+++ app/models/dept.rb
@@ -1,6 +1,10 @@
 class Dept < ApplicationRecord
   belongs_to :dl, foreign_key: 'IdDl'
-
+  
   self.table_name = 'Dl_Dept'
   self.primary_key = 'IdDl'
+
+  def manager
+    Role.find(self[:IdManager]).user
+  end
 end
