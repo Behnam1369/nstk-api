751d5972e62aafce5289c3d1aaa081489f79fc2b
jamesbaylor <jamesbay829@gmail.com>
2023-05-22 17:32:19 +0430

    Update User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
 class User < ApplicationRecord
   has_many :work_missioners, foreign_key: 'IdUser'
   has_many :roles, foreign_key: 'IdUser'
+  has_many :user_group_members, foreign_key: :IdUser
+  has_many :user_groups, through: :user_group_members
 
   self.table_name = 'Users'
   self.primary_key = 'IdUser'
+
+  def full_name 
+    self.Fname + ' ' + self.Lname
+  end
+
+  def dept
+    self.roles.first.dept
+  end
 end
