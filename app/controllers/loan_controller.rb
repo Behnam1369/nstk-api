01092877a788ed777251c9360cb28715a0a928eb
jamesbaylor <jamesbay829@gmail.com>
2023-05-24 14:56:17 +0430

    Update Loan controller

--- app/controllers/loan_controller.rb
+++ app/controllers/loan_controller.rb
@@ -34,7 +34,9 @@ include ShamsiDateHelper
         dbo.HierarchyLevel(iduser) as HierarchyLevel,
         dbo.ActiveMonthlyInstallments(idwinkart) as ActiveMonthlyInstallments,
         fname + ' ' + lname as FullName , 
-        IdWinkart as PerNo
+        IdWinkart as PerNo,
+        (select string_agg(idrole, ',') from role where iduser = #{params[:iduser]}) as Roles,
+        (select string_agg(idgroup, ',') from UserGroupMembers where iduser = #{params[:iduser]}) as UserGroups
         from users where iduser = @iduser for json path, include_null_values) as nvarchar(max)) as data
     "
 
@@ -60,7 +62,8 @@ include ShamsiDateHelper
     manager = User.find(params[:iduser]).dept.manager
     @loan.IdManager = manager.IdUser
     @loan.Manager = manager.full_name
-    @loan.Step = 2
+    @loan.IdStep = 2
+    @loan.Step = 'در حال بررسی توسط مدیر واحد'
     @loan.RequestDate = DateTime.now
     @loan.RequestDateShamsi = shamsi_date(DateTime.now)
 
@@ -74,7 +77,8 @@ include ShamsiDateHelper
   def send_to_hr
     @loan = Loan.find(params[:idloan])
     @loan.ManagerNote = params[:Note]
-    @loan.Step = 3
+    @loan.IdStep = 3
+    @loan.Step = 'در حال بررسی توسط منابع انسانی'
     @loan.ManagerConfirmDate = DateTime.now
     @loan.ManagerConfirmDateShamsi = shamsi_date(DateTime.now)
     hr_manager = Role.find(55).user
@@ -91,7 +95,8 @@ include ShamsiDateHelper
   def send_to_ceo
     @loan = Loan.find(params[:idloan])
     @loan.HrNote = params[:Note]
-    @loan.Step = 4
+    @loan.IdStep = 4
+    @loan.Step = 'در انتظار تایید مدیرعامل'
     @loan.HrConfirmDate = DateTime.now
     @loan.HrConfirmDateShamsi = shamsi_date(DateTime.now)
     ceo = Role.find(2).user
@@ -111,14 +116,30 @@ include ShamsiDateHelper
   def send_to_finance
     @loan = Loan.find(params[:idloan])
     @loan.CeoNote = params[:Note]
-    @loan.Step = 5
+    @loan.IdStep = 5
+    @loan.Step = 'در انتظار پرداخت'
     @loan.CeoConfirmDate = DateTime.now
     @loan.CeoConfirmDateShamsi = shamsi_date(DateTime.now)
     finance_users = UserGroup.find(109).users.map{ |user|  user.IdUser }.join(",")
     @loan.IdFin = finance_users
 
-    puts ceo.inspect
-    puts @loan.inspect
+    if @loan.save
+      render json: { message: 'Success', loan: @loan }
+    else
+      render json: { message: 'Error', data: @loan.errors }
+    end
+  end
+
+  def confirm_payment
+    puts params.inspect
+    @loan = Loan.find(params[:idloan])
+    @loan.FinNote = params[:Note]
+    @loan.IdStep = 6
+    @loan.Step = 'پرداخت شده'
+    @loan.State = 'Paid'
+    @loan.Fin = User.find(params[:iduser]).full_name
+    @loan.InstallmentFirstMonth = params[:InstallmentFirstMonth]
+    @loan.InstallmentLastMonth = params[:InstallmentLastMonth]
     
     if @loan.save
       render json: { message: 'Success', loan: @loan }
@@ -163,7 +184,7 @@ include ShamsiDateHelper
     @loan.CeoConfirmDate = DateTime.now
     @loan.CeoConfirmDateShamsi = shamsi_date(DateTime.now)
     @loan.Step = -1
-    @loan.State = 'عدم تایید توسط ' + @loan.ceo
+    @loan.State = 'عدم تایید توسط ' + @loan.Ceo
     if @loan.save
       render json: { message: 'Success', loan: @loan }
     else
@@ -179,6 +200,7 @@ include ShamsiDateHelper
       :PerNo,
       :IdLoanType,
       :LoanType,
+      :IdStep,
       :Step,
       :State,
       :RequestDate,
